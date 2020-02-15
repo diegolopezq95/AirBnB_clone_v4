c483d7c439a82fb6b831938e60cd73dc64f765bc
jorgezafra94 <947@holbertonschool.com>
2020-02-15 19:53:38 +0000

    [fix] adding status of API

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -11,6 +11,15 @@ document.addEventListener('DOMContentLoaded', function () {
         $('.amenities h4').text(Object.values(myDict).join(', '));
       }
     });
+
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+      if (data.status === 'OK') {
+        $('DIV#api_status').addClass('available');
+      } else {
+        $('DIV#api_status').removeClass('available');
+      }
+    });
+
     $.ajax({
       url: 'http://0.0.0.0:5001/api/v1/places_search/',
       contentType: 'application/json',
