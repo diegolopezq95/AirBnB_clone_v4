61879fffaa3be88b21b9ce968cd0515b7524d481
jorgezafra94 <947@holbertonschool.com>
2020-02-15 17:58:33 +0000

    [feature] getting places from frontend not backend

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function () {
+  $(function () {
+    const myDict = {};
+    $('input[type="checkbox"]').click(function () {
+      if ($(this).is(':checked')) {
+        myDict[$(this).attr('data-id')] = $(this).attr('data-name');
+        console.log($(this).attr('data-id'));
+        $('.amenities h4').text(Object.values(myDict).join(', '));
+      } else if ($(this).is(':not(:checked)')) {
+        delete myDict[$(this).attr('data-id')];
+        $('.amenities h4').text(Object.values(myDict).join(', '));
+      }
+    });
+    $.ajax({
+      url: 'http://localhost:5001/api/v1/places_search/',
+      contentType: 'application/json',
+      type: 'POST',
+      data: JSON.stringify({ }),
+      success: function (data) {
+        for (const elem of Object.values(data)) {
+          console.log(elem);
+          $('SECTION.places').append(
+            '<article> <div class="title">' +
+                '<h2>' + elem.name + '</h2>' +
+                '<div class="price_by_night">' + elem.price_by_night + '</div> </div>' +
+                '<div class="information"> <div class="max_guest">' +
+                '<i class="fa fa-users fa-3x" aria-hidden="true"></i>' +
+                '<br />' + elem.max_guest + 'Guests' + '</div>' +
+                '<div class="number_rooms">' +
+                '<i class="fa fa-bed fa-3x" aria-hidden="true"></i>' +
+                '<br />' + elem.number_rooms + 'Bedrooms' + '</div>' +
+                '<div class="number_bathrooms">' +
+                '<i class="fa fa-bath fa-3x" aria-hidden="true"></i>' +
+                '<br />' + elem.number_bathrooms + 'Bathroom' + '</div>' +
+                '</div>' + '</article>'
+          );
+        }
+      }
+    });
+  });
+});
